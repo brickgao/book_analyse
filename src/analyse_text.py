1fe42c6f0f92f4e8a6f92dca25de9b5a80ad9cb0
brickgao <brickgao@gmail.com>
2014-09-18 17:16:50 +0800

    finish get_realtionship

--- src/analyse_text.py
+++ src/analyse_text.py
@@ -2,6 +2,33 @@
 
 import word2vec
 
+name_list = [u'何塞',
+              u'阿尔卡蒂奥', 
+              u'布恩迪亚', 
+              u'乌尔苏拉',
+              u'伊瓜兰',
+              u'奥雷里亚诺',
+              u'布恩迪亚',
+              u'阿玛兰妲',
+              u'丽贝卡',
+              u'蕾梅黛丝',
+              u'摩斯科特',
+              u'阿尔卡蒂奥',
+              u'奥雷里亚诺',
+              u'蕾梅黛丝',
+              u'费尔南达',
+              u'德尔',
+              u'卡皮奥',
+              u'佩特拉',
+              u'克特斯',
+              u'雷纳塔',
+              u'蕾梅黛丝',
+              u'阿玛兰妲',
+              u'奥雷里亚诺',
+              u'梅尔加德斯',
+              u'庇拉尔',
+              u'特尔内拉']
+
 def training():
     '''
         Training use file in '../tmp/book-seg.txt'.
@@ -13,35 +40,23 @@ def cosine_all():
     '''
         Use model to cosine all name in the book.
     '''
-    _name_list = [u'何塞',
-                  u'阿尔卡蒂奥', 
-                  u'布恩迪亚', 
-                  u'乌尔苏拉',
-                  u'伊瓜兰',
-                  u'奥雷里亚诺',
-                  u'布恩迪亚',
-                  u'阿玛兰妲',
-                  u'丽贝卡',
-                  u'蕾梅黛丝',
-                  u'摩斯科特',
-                  u'阿尔卡蒂奥',
-                  u'奥雷里亚诺',
-                  u'蕾梅黛丝',
-                  u'费尔南达',
-                  u'德尔',
-                  u'卡皮奥',
-                  u'佩特拉',
-                  u'克特斯',
-                  u'雷纳塔',
-                  u'蕾梅黛丝',
-                  u'阿玛兰妲',
-                  u'奥雷里亚诺',
-                  u'梅尔加德斯',
-                  u'庇拉尔',
-                  u'特尔内拉']
+    _name_list = name_list
     map(lambda _: _.encode('utf-8'), _name_list)
     model = word2vec.load('../tmp/book.bin')
     return model.cosine(_name_list, n=10)
+
+def get_relation(_dict):
+    '''
+        Get a realtionship among people.
+    '''
+    _ret_d = {}
+    for _name in name_list:
+        _ret_d[_name] = []
+    for _name in _dict.keys():
+        for _e in _dict[_name]:
+            if _e[0] in name_list:
+                _ret_d[_name].append(_e[0])
+    return _ret_d
 
 
 if __name__ == '__main__':
