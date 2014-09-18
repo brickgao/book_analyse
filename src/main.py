833f21de6b8612b2ef56b68b1bd50759d510d807
brickgao <brickgao@gmail.com>
2014-09-18 20:08:28 +0800

    add serve func

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import analyse_text, draw, text_segmentation
+import analyse_text, draw, text_segmentation, os, SimpleHTTPServer, SocketServer
 
 if __name__ == '__main__':
     text_segmentation.text_seg()
@@ -8,3 +8,8 @@ if __name__ == '__main__':
     _ = analyse_text.cosine_all()
     _ = analyse_text.get_relationship(_)
     draw.draw(_)
+    os.chdir('../tmp')
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", 8000), Handler)
+    print 'serving at port 8000'
+    httpd.serve_forever()
