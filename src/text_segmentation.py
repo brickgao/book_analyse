b5ca19cb8c19e4f79e236a588c1b45625bbabf43
brickgao <brickgao@gmail.com>
2014-09-18 16:18:59 +0800

    change defualt param of text_seg

--- src/text_segmentation.py
+++ src/text_segmentation.py
@@ -2,15 +2,17 @@
 
 import jieba
 
-def text_seg():
+def text_seg(file_in_path='../tmp/book-utf-8.txt',
+             file_out_path='../tmp/book-seg.txt',
+             user_dict_path='../tmp/user_dict_path.txt')
     '''
         Use segmentation module to cut the word.
-        You can find user dict in '../tmp/userdict.txt'.
-        File_in: '../tmp/book-utf-8.txt'
-        File_out: '../tmp/book-seg.txt'
+        You can find default user dict in '../tmp/userdict.txt'.
+        Default_File_in: '../tmp/book-utf-8.txt'
+        Default_File_out: '../tmp/book-seg.txt'
     '''
-    jieba.load_userdict("../tmp/userdict.txt")
-    with file('../tmp/book-utf-8.txt') as _f_in:
+    jieba.load_userdict(user_dict_path)
+    with file(file_in_path) as _f_in:
         while True:
             _line_in = _f_in.readline()
             if _line_in == '': break
@@ -18,7 +20,7 @@ def text_seg():
             # make cuted words separated with space
             _line_out = u' '.join(_seg_list)
             _line_out = _line_out.encode('utf-8')
-            with file('../tmp/book-seg.txt', 'a') as _f_out:
+            with file(file_out_path, 'a') as _f_out:
                 _f_out.write(_line_out)
 
 if __name__ == '__main__':
